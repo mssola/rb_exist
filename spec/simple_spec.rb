79b9a474bd4eecb5b791b862f219886e2d53188e
Miquel Sabaté <mikisabate@gmail.com>
2011-10-01 00:03:26 +0200

    Test suite expanded so it now covers all the cases for the select query

--- spec/simple_spec.rb
+++ spec/simple_spec.rb
@@ -1,25 +1,52 @@
 
+require 'libxml'
+
+##
+# Helper method useful to compare simple XML files.
+#
+# @param *LibXML::XML::Document* xml The xml file from a query.
+#
+# @param *String* test_name The name of the test.
+#
+# @param *Array* params The nodes that the XML parser should take care.
+#
+# @return *Boolean* true if both xml files are identical, false otherwise
+def compare_xml(xml, test_name, params)
+  result = false
+  p = LibXML::XML::Parser.file test_name
+  result_p = LibXML::XML::Parser.string xml.to_s
+  doc = p.parse
+  result_d = result_p.parse
+  params.each do |p|
+    a = doc.find("//#{p}").to_a
+    b = result_d.find("//#{p}").to_a
+    return false if a.size != b.size
+    a.size.times { |i| return false if a[i].content != b[i].content }
+  end
+  true
+end
+
+# Let the testing begin!
+
 describe 'SimpleSQL' do
   it 'executes the select query correctly' do
     db = Exist::ExistDB.new("#{$server_ip}/db")
-    table = IO.read(File.dirname(__FILE__) + '/data/users.xml')
+    base =File.dirname(__FILE__) + '/data/'
+    table = IO.read(base + 'users.xml')
     db.store 'users.xml', table
     sql = db.simple_sql
-    sql.select(:from => 'users')
+    xml = sql.select(:from => 'users')
+    compare_xml(xml, base + 'users.xml', ['name', 'age']).should eql(true)
     sql.count.should eql('3')
-    sql.select(:from => 'users',
-               :where => 'age>20')
-    sql.count.should eql('1')
-    sql.select(:from => 'users',
-               :where => 'age<19')
-    sql.count.should eql('1')
-    sql.select(:from => 'users',
-               :where => 'age>19')
+
+    xml= sql.select(:from => 'users', :where => 'age>19')
+    compare_xml(xml, base + 'users_rev.xml', ['name', 'age']).should eql(true)
     sql.count.should eql('2')
-    xml = sql.select(:from => 'users',
-               :where => '')
-    sql.count.should eql('3')
-    xml = sql.select(:rows => ['age'], :from => 'users')
+
+    xml = sql.select(rows: ['age'], from: 'users',
+                   where: "name/text()!='Pedobear'",
+                   order_by: 'name_ascending')
+    (IO.read(base + 'order_by.xml').strip == xml.to_s.strip).should eql(true)
     db.delete 'users.xml'
   end
 
